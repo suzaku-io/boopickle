dad5cb3c5d16d2d3bb3b2c07084ef422b8e55ee3
Stephane Landelle <slandelle@gatling.io>
2017-02-21 13:15:13 +0100

    Fix ByteBufferProvider#asByteBuffers order, close #72
    Motivation:
    
    `ByteBufferProvider#asByteBuffers` currently returns ByteBuffers is
    reverse order, which is unexpected.
    One would expect them to be in proper temporal order.
    
    Modifications:
    
    * Reverse ByteBuffer sequence order before returning
    * Add test proving proper behavior
    
    Result:
    
    ByteBufferProvider#asByteBuffers now returns in proper order

---
+++ boopickle/shared/src/test/scala/boopickle/BufferProviderTests.scala
@@ -0,0 +1,28 @@
+package boopickle
+
+import java.nio.ByteBuffer
+
+import scala.util.Random
+
+import external.Banana
+import utest._
+import boopickle.Default._
+
+object BufferProviderTests extends TestSuite {
+
+  override def tests = TestSuite {
+    'asByteBuffersProperOrder {
+
+      val input: Seq[Banana] = Iterator.tabulate(100)(_ => Banana(Random.nextDouble)).toVector
+      val bbs = Pickle.intoByteBuffers(input)
+      assert(bbs.size > 1)
+
+      val mergedBb = ByteBuffer.allocate(bbs.map(_.remaining).sum)
+      bbs.foreach(mergedBb.put)
+      mergedBb.flip()
+
+      val output = Unpickle[Seq[Banana]].fromBytes(mergedBb)
+      assert(output == input)
+    }
+  }
+}
